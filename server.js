22a7b39ca1f3f49816c250ade0034c0b5712b25b
victorbofill <victorm.bofill@gmail.com>
2018-04-15 16:11:20 -0700

    progress

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const http = require('http');
+const url = require('url');
 
-http.createServer(function(request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}).listen(8888);
+function start(route, handle) {
+    function onRequest(request, response) {
+        const pathname = url.parse(request.url).pathname;
+        console.log("Request for " + pathname + " received.");
+
+        route(handle, pathname);
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello world!");
+        response.end();
+    }
+    
+    http.createServer(onRequest).listen(8888);    
+    console.log("Server has started.");
+}
+
+exports.start = start;
